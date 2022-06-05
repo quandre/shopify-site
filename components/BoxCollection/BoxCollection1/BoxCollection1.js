166a4f1d9b26c346f1110b5393d3d73c3255c024
Quandre <Quandremyers@yahoo.com>
2022-06-05 19:52:01 -0400

    update

--- components/BoxCollection/BoxCollection1/BoxCollection1.js
+++ components/BoxCollection/BoxCollection1/BoxCollection1.js
@@ -1,52 +1,62 @@
 export default function BoxCollection1() {
   return (
     <div className="box-collection1">
-      <div className="box-collection1__left">
-        <div className="box-collection1__left-top">
-          <img
-            src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
-            alt=""
-          />
-          <div className="box-collection1__left-box">
-            <p className="box-collection1__left-box-header">Title</p>
-            <p className="box-collection1__left-box-text">Text</p>
+      <div className="box-collection1__container">
+        <div className="box-collection1__left">
+          <div className="box-collection1__left-top">
+            <img
+              src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
+              alt=""
+            />
+            <div className="box-collection1__top-content-container">
+              <div className="box-collection1__left-box">
+                <p className="box-collection1__left-box-header">Title</p>
+                <p className="box-collection1__left-box-text">Text</p>
+                <button>Shop Now</button>
+              </div>
+            </div>
           </div>
-          <button>Shop Now</button>
+          <div className="box-collection1__left-bottom">
+            <img
+              src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
+              alt=""
+            />
+            <div className="box-collection1__bottom-content-container">
+              <div className="box-collection1__left-box">
+                <p className="box-collection1__left-box-header">Title</p>
+                <p className="box-collection1__left-box-text">Text</p>
+                <button>Shop Now</button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="box-collection1__left-bottom">
-          <img
-            src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
-            alt=""
-          />
-          <div className="box-collection1__left-box">
-            <p className="box-collection1__left-box-header">Title</p>
-            <p className="box-collection1__left-box-text">Text</p>
+        <div className="box-collection1__right">
+          <div className="box-collection1__right-top">
+            <img
+              src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
+              alt=""
+            />
+            <div className="box-collection1__top-content-container">
+              <div className="box-collection1__right-box">
+                <p className="box-collection1__right-box-header">Title</p>
+                <p className="box-collection1__right-box-text">Text</p>
+                <button>Shop Now</button>
+              </div>
+            </div>
           </div>
-          <button>Shop Now</button>
-        </div>
-      </div>
-      <div className="box-collection1__right">
-        <div className="box-collection1__right-top">
-          <img
-            src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
-            alt=""
-          />
-          <div className="box-collection1__right-box">
-            <p className="box-collection1__right-box-header">Title</p>
-            <p className="box-collection1__right-box-text">Text</p>
+          <div className="box-collection1__right-bottom">
+            <img
+              src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
+              alt=""
+            />
+            <div className="box-collection1__bottom-content-container">
+              <div className="box-collection1__right-box">
+                <p className="box-collection1__right-box-header">Title</p>
+                <p className="box-collection1__right-box-text">Text</p>
+                <button>Shop Now</button>
+              </div>
+            </div>
           </div>
-          <button>Shop Now</button>
-        </div>
-        <div className="box-collection1__right-bottom">
-          <img
-            src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
-            alt=""
-          />
-          <div className="box-collection1__right-box">
-            <p className="box-collection1__right-box-header">Title</p>
-            <p className="box-collection1__right-box-text">Text</p>
-          </div>
-          <button>Shop Now</button>
         </div>
       </div>
     </div>
