e747b4f3dc557e56c39cea18d8db4dc85915acea
Quandre <Quandremyers@yahoo.com>
2022-06-06 02:31:31 -0400

    BoxCollection done

--- components/BoxCollection/BoxCollection1/BoxCollection1.js
+++ components/BoxCollection/BoxCollection1/BoxCollection1.js
@@ -3,12 +3,12 @@ export default function BoxCollection1() {
     <div className="box-collection1">
       <div className="box-collection1__container">
         <div className="box-collection1__left">
-          <div className="box-collection1__left-top">
+          <div className="box-collection1__left-top box-collection1--aligned-left box-collection1--cover box-collection1--img-center">
             <img
               src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
               alt=""
             />
-            <div className="box-collection1__top-content-container">
+            <div className="box-collection1__content-container">
               <div className="box-collection1__left-box">
                 <p className="box-collection1__left-box-header">Title</p>
                 <p className="box-collection1__left-box-text">Text</p>
@@ -16,12 +16,13 @@ export default function BoxCollection1() {
               </div>
             </div>
           </div>
-          <div className="box-collection1__left-bottom">
+          <div className="box-collection1__left-bottom box-collection1--aligned-left box-collection1--cover
+          box-collection1--img-center">
             <img
               src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
               alt=""
             />
-            <div className="box-collection1__bottom-content-container">
+            <div className="box-collection1__content-container">
               <div className="box-collection1__left-box">
                 <p className="box-collection1__left-box-header">Title</p>
                 <p className="box-collection1__left-box-text">Text</p>
@@ -31,12 +32,13 @@ export default function BoxCollection1() {
           </div>
         </div>
         <div className="box-collection1__right">
-          <div className="box-collection1__right-top">
+          <div className="box-collection1__right-top box-collection1--aligned-left box-collection1--contain
+          box-collection1--img-right">
             <img
               src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
               alt=""
             />
-            <div className="box-collection1__top-content-container">
+            <div className="box-collection1__content-container">
               <div className="box-collection1__right-box">
                 <p className="box-collection1__right-box-header">Title</p>
                 <p className="box-collection1__right-box-text">Text</p>
@@ -44,12 +46,12 @@ export default function BoxCollection1() {
               </div>
             </div>
           </div>
-          <div className="box-collection1__right-bottom">
+          <div className="box-collection1__right-bottom box-collection1--aligned-left box-collection1--cover box-collection1--img-center">
             <img
               src="https://m.media-amazon.com/images/I/71xMzz84JfS._AC_SL1500_.jpg"
               alt=""
             />
-            <div className="box-collection1__bottom-content-container">
+            <div className="box-collection1__content-container">
               <div className="box-collection1__right-box">
                 <p className="box-collection1__right-box-header">Title</p>
                 <p className="box-collection1__right-box-text">Text</p>
